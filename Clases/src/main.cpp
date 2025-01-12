43ff09d65e8ac0e2580e6e7c93d0a7598a5d873d
victor <victormmla3lm@gmail.com>
2025-01-12 16:25:56 -0300

    Mal ejemplo de polimorfismo

--- Clases/src/main.cpp
+++ Clases/src/main.cpp
@@ -9,8 +9,21 @@ class ClaseInterna {
         }
 } instanciaInterna;
 
+class ClaseInternaHija final : public ClaseInterna {
+    public:
+        ClaseInternaHija() = default;
+    public:
+        // mal uso del polimorfismo
+        std::string mensaje() const
+        {
+            return "Clase Hija de la clase interna.\n";
+        }
+};
+
 int main(void)
 {
     std::cout << instanciaInterna.mensaje();
+    ClaseInternaHija hija;
+    std::cout << hija.mensaje();
     return EXIT_SUCCESS;
 }
