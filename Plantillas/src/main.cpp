83dec25434a91ac5ad3482336e18be68695c479d
victor <victormmla3lm@gmail.com>
2025-01-15 14:38:15 -0300

    Se agregó una función amiga con platilla

--- Plantillas/src/main.cpp
+++ Plantillas/src/main.cpp
@@ -7,8 +7,11 @@ class ClaseGenerica {
     public:
         ClaseGenerica(T valor) : valor(valor) {};
     public:
-        T demo() { return valor; }
+        T getValor() { return valor; }
     private: T valor;
+
+    template <typename U>
+    friend void mostrarValor(ClaseGenerica<U> g);
 };
 
 template <typename T>
@@ -17,16 +20,23 @@ class ClaseHija final : public ClaseGenerica<T> {
         ClaseHija(T valor) : ClaseGenerica<T>(valor) {};
 };
 
+template <typename T>
+void mostrarValor(ClaseGenerica<T>* g) {
+    std::cout << "valor almacenado en clase genérica: " << g->getValor() << '\n';
+}
+
 int main(void) {
     // primera instancia
     ClaseGenerica<std::string> cadena("cadena");
-    std::cout << "valor: " << cadena.demo() << std::endl;
+    std::cout << "valor: " << cadena.getValor() << std::endl;
     // segunda instancia
     ClaseGenerica<int> numero(-23);
-    std::cout << "valor: " << numero.demo() << std::endl;
+    std::cout << "valor: " << numero.getValor() << std::endl;
     // clase hija
     ClaseGenerica<std::string>* puntero = new ClaseHija<std::string>("Otra cadena");
-    std::cout << "valor de la clase hija: " << puntero->demo() << std::endl;
+    std::cout << "valor de la clase hija: " << puntero->getValor() << std::endl;
+    // uso de la función amiga
+    mostrarValor(puntero);
     delete puntero;
     puntero = nullptr;
     return EXIT_SUCCESS;
