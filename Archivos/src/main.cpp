96a0c06ceff93fc3f8f223a1e563641b7e51999b
victor <victormmla3lm@gmail.com>
2025-01-18 23:08:46 -0300

    Algunos comentarios para recordar

--- Archivos/src/main.cpp
+++ Archivos/src/main.cpp
@@ -4,14 +4,19 @@
 
 int main(void) {
     std::string nombreDeArchivo = "archivo.txt";
+    // abro el archivo en su configuración por defecto
     std::ofstream salida(nombreDeArchivo);
+
     std::cout << "'CTRL+D' o 'CTRL+Z' para guardar todo y salir.\n";
     std::cout << "Ingrese lo que desea guardar en el archivo: ";
+
     char dato;
     while (std::cin.get(dato)) {
-        salida.put(dato);
+        salida.put(dato); // escribe los datos directamente en el archivo
     }
+
     salida.close();
+
     std::cout << "Datos almacenados en \"" << nombreDeArchivo << "\"\nFin del programa." << std::endl;
     return EXIT_SUCCESS;
 }
--- ArchivosBinarios/src/main.cpp
+++ ArchivosBinarios/src/main.cpp
@@ -4,19 +4,29 @@
 
 int main(void) {
     std::string nombreDeArchivo = "archivo.dat";
+    // abro el archivo y especifico que sea en modo binario (eliminando lo que ya tenga)
     std::ofstream salida(nombreDeArchivo, std::ios::out | std::ios::binary);
+
     if (!salida.is_open())
         return EXIT_FAILURE;
+    
     std::cout << "Presione enter y luego 'CTRL+D' o 'CTRL+Z' para terminar.\n";
     std::cout << "Ingrese lo que desea guardar en el archivo: ";
+
     char dato;
     while (std::cin.get(dato)) {
-        salida.write(&dato, sizeof(dato));
+        // de esta forma no es necesario escificar los saltos de línea
+        salida.write(&dato, sizeof(dato)); // si presiono enter y luego CTRL + D se termina este ciclo
     }
+
     salida.close();
+
+    // ahora abro el archivo y especifico que sea en modo binario
     std::ifstream entrada(nombreDeArchivo, std::ios::in | std::ios::binary);
+
     if (!entrada.is_open())
         return EXIT_FAILURE;
+    
     int linea = 1;
     std::cout << linea << " | ";
     while (entrada.get(dato)) {
@@ -27,5 +37,6 @@ int main(void) {
         }
     }
     entrada.close();
+
     return EXIT_SUCCESS;
 }
